a881d77d7b5e434f167ec7ba89c8046e0c77d7d3
Shashi Raj <shraj@groupon.com>
2020-08-29 19:52:00 +0530

    fix

--- drone.py
+++ drone.py
@@ -1,6 +1,5 @@
 from utils import Utils
 from interval import Interval
-import sys
 
 
 class Drone:
@@ -17,13 +16,8 @@ class Drone:
         for line in lines:
             data = line.strip('\n').strip().split('/')
             # list index starts from 0
-            self.intervals[int(data[0].strip()) - 1] = Interval(int(data[0].strip()), int(data[1].strip()), int(data[2].strip()))
-
-        self.outputResultList.append("initial data:")
-        self.outputResultList.append("Id    manufacturing time      flight testing time")
-        for interval in self.intervals:
-            self.outputResultList.append(str(interval.getId()) + '\t' + str(interval.getManufacturingTime()) + '\t' + str(
-                interval.getFlightTestingTime()))
+            self.intervals[int(data[0].strip()) - 1] = Interval(int(data[0].strip()), int(data[1].strip()),
+                                                                int(data[2].strip()))
 
     def scheduleTask(self):
         """
@@ -32,34 +26,41 @@ class Drone:
         :return:
         """
         # we need to schedule all the drone in minimum time
-        # sorting based on manufacturing Time
+        # sorting based on increasing order of manufacturing Time
         scheduled_tasks = []  # list to represent all the scheduled task
         interval_list = list(self.intervals)
-        interval_list.sort(key=lambda x: x.manufacturingTime)  # so that we know which drone will be manufactured first
-        self.outputResultList.append("sorted data")
-        self.outputResultList.append("Id    manufacturing time      flight testing time")
-        for interval in interval_list:
-            self.outputResultList.append(
-                str(interval.getId()) + '\t' + str(interval.getManufacturingTime()) + '\t' + str(interval.getFlightTestingTime()))
+        interval_list.sort(
+            key=lambda x: x.manufacturingTime)  # so that we know which drone will be manufactured first based on time
 
         id_list = []
         for interval in interval_list:
             id_list.append(str(interval.getId()))
 
-        self.outputResultList.append("Drones should be produced in the order:"+ ','.join(id_list))
-        last = -sys.maxsize
-        for interval in interval_list:
-            if last > 0:
-                for i in range(interval.getManufacturingTime() - last):
-                    scheduled_tasks.append('IDLE')
+        self.outputResultList.append("Drones should be produced in the order:" + ','.join(id_list))
 
+        # now we need to schedule all the task and corresponding for the flight testing
+        flight_testing_schedule = []
+        last_flight_testing_time = None
+        for interval in interval_list:
+            # this is total time for manufacturing
             for time_unit in range(interval.getManufacturingTime()):
                 scheduled_tasks.append(interval.getId())
 
-            last = interval.getFlightTestingTime()
+            # now we need to schedule for flight testing
+            if last_flight_testing_time is not None:
+                while last_flight_testing_time <= interval.getManufacturingTime():
+                    flight_testing_schedule.append('IDLE')
+                    last_flight_testing_time += 1
 
-        self.outputResultList.append(str(scheduled_tasks))
-        self.outputResultList.append(str(len(scheduled_tasks)))
+            for time_unit in range(interval.getFlightTestingTime()):
+                flight_testing_schedule.append(interval.getId())
+
+            last_flight_testing_time = len(flight_testing_schedule)
+
+        self.outputResultList.append(
+            'Total production time for all drones is:' + str(max(len(flight_testing_schedule), len(scheduled_tasks))))
+        idle_time_unit = lambda x: x == 'IDLE', flight_testing_schedule
+        self.outputResultList.append('Idle time of flight testing unit:' + str(len(idle_time_unit)))
         self.utils.writeToOutputFile(self.outputFile, self.outputResultList)
 
 
